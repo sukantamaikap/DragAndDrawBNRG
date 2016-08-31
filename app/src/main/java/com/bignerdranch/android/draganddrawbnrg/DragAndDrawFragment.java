e66fae21976816a68d0de74a841525cb6209898d
sukantamaikap <sukanta.maikap@gmail.com>
2016-08-31 21:54:44 +0530

    chapter 29, basic elements setup

---
+++ app/src/main/java/com/bignerdranch/android/draganddrawbnrg/DragAndDrawFragment.java
@@ -0,0 +1,27 @@
+package com.bignerdranch.android.draganddrawbnrg;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by smaikap on 31/8/16.
+ */
+public class DragAndDrawFragment extends Fragment {
+
+    public static DragAndDrawFragment newInstance() {
+        return new DragAndDrawFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(final LayoutInflater inflater,
+                             @Nullable final ViewGroup container,
+                             @Nullable final Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_drag_and_draw, container, false);
+        return view;
+    }
+}
