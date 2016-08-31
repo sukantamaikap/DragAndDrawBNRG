b12e3990a5a51898a85a6c91d25728fa9bdea61f
sukantamaikap <sukanta.maikap@gmail.com>
2016-08-31 22:02:39 +0530

    chapter 29 added basic of custom view

---
+++ app/src/main/java/com/bignerdranch/android/draganddrawbnrg/BoxDrawingView.java
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.draganddrawbnrg;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by smaikap on 31/8/16.
+ */
+public class BoxDrawingView extends View {
+
+    public BoxDrawingView(final Context context) {
+        this(context, null);
+    }
+
+    public BoxDrawingView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
